10a8715deddb3a536cae83e0377225a116372d19
Aakash Podar <aakashpodar.ce21@sltiet.edu.in>
2024-07-25 16:27:53 +0530

    initial

---
+++ spotify-backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config';
+import songRouter from "./src/routes/songRoute.js";
+import connectDB from "./src/config/mongodb.js";
+import connectCloudinary from "./src/config/cloudinary.js";
+
+// app config
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+
+
+// middlewares
+// express.json() are use to make text-based format to send and receive msg
+app.use(express.json());
+app.use(cors());
+
+
+// initializing routes
+
+app.use("/api/song" , songRouter)
+
+
+app.get('/' ,(req,res) => {
+    res.send("API WORKING ON PORT 4000");
+})
+
+
+// listining the requset
+
+app.listen(port ,()=>{
+    console.log(`Server Started on port ${port}`);
+})
---
+++ spotify-backend/src/models/songModel.js
@@ -0,0 +1,42 @@
+
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    decs: {
+        type: String,
+        required: true
+    },
+
+    album: {
+        type: String,
+        required: true,
+
+    }
+    ,
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    file: {
+        type: String,
+        required: true
+    },
+
+    duration: {
+        type: String,
+        required: true
+    }
+});
+
+// const songModel = mongoose.model('songs' , songSchema);
+
+const songModel = mongoose.models.song || mongoose.model("song" , songSchema);
+
+export default songModel ; 
