9485e9707f5f9dc2b5d9b23550552b06718702de
Aakash Podar <aakashpodar.ce21@sltiet.edu.in>
2024-08-01 19:48:21 +0530

    add

--- spotify-backend/src/config/cloudinary.js
+++ spotify-backend/src/config/cloudinary.js
@@ -2,15 +2,13 @@ import { v2 as cloudinary } from 'cloudinary'
 
 
 const connectCloudinary = async () => {
-
- console.log("cloudinary is succesfully connectd");
+    console.log("cloudinary is succesfully connectd");
 
     await cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME ,
+        cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret_key: process.env.CLOUDINARY_API_SECRET_KEY
-
-    });   
+    });
 }
 
 export default connectCloudinary;
